4233b786f933613a081e09839b8adb12e855bdb0
David Cho <davidscho9@gmail.com>
2020-02-05 10:50:27 -0800

    done

--- for_loop.py
+++ for_loop.py
@@ -4,6 +4,11 @@ for item in price:
     cost += item
 print(f"Total: {cost}")
 
+# matrix
 for x in range(4):
     for y in range(3):
         print(f"({x}, {y})")
+
+numbers = [5, 2, 5, 2, 2]
+for x in range(len(numbers)):
+    print("x" * numbers[x])
