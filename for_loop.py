636a9c442b72a488de0f687a2975a9657c5e002b
David Cho <davidscho9@gmail.com>
2020-02-05 11:16:44 -0800

    done

--- for_loop.py
+++ for_loop.py
@@ -10,5 +10,15 @@ for x in range(4):
         print(f"({x}, {y})")
 
 numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print("x" * x_count)
+
+for x_count in numbers:
+    output = ''     #this resets output variable to empty string
+    for count in range(x_count):    #inner loop executed 5 times
+        output += 'x'
+    print(output)
+
+
 for x in range(len(numbers)):
     print("x" * numbers[x])
